428453a1a138e26f3fecaa51e6609e3542b0fc16
progg8 <150245678+progg8@users.noreply.github.com>
2023-11-08 13:07:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("demo").addEventListener("click", function(){
+    document.getElementById("modal").style.display = "flex"
+})
+document.getElementById("cros").addEventListener("click", function(){
+    document.getElementById("modal").style.display = "none"
+})
